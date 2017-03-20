0b966494cb3df81e11983d82dff3531c6a08342e
Tobey Unruh <Tobey@tobeys-macbook-pro.local>
2017-03-20 16:44:49 -0400

    Initial Commit

---
+++ FXR/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  FXR
+//
+//  Created by Tobey Unruh on 3/20/17.
+//  Copyright © 2017 Tobey Unruh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
